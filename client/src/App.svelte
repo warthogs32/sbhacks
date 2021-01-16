d5026b0fbbb4194525174620c2fcf21bfc69e7df
Eric Y <eye@usc.edu>
2021-01-16 13:54:23 -0700

    Created new branch

---
+++ client/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Router, Link, Route } from "svelte-routing";
+  import Welcome from "./Welcome.svelte";
+
+  export let url = "";
+</script>
+
+<Router {url}>
+  <div>
+    <!-- <Route path="about" component="{About}" /> -->
+    <Route path="/"><Welcome /></Route>
+  </div>
+</Router>
