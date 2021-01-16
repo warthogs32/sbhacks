aec72e8e618b043d5279f82faad8904368286350
Alan Chen <azc242@nyu.edu>
2021-01-16 12:14:03 -0800

    set up basic routing and unstyled incomplete welcome page

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,30 +1,13 @@
 <script>
-	export let name;
+  import { Router, Link, Route } from "svelte-routing";
+  import Welcome from "./Welcome.svelte";
+
+  export let url = "";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Router {url}>
+  <div>
+    <!-- <Route path="about" component="{About}" /> -->
+    <Route path="/"><Welcome /></Route>
+  </div>
+</Router>
