b84c66243e278357f49ac79cc869c6fec89774b4
Alan Chen <azc242@nyu.edu>
2021-01-16 22:01:27 -0800

    style analytics page for responsiveness (needs testing on ipad)

--- client/src/routes/analytics.svelte
+++ client/src/routes/analytics.svelte
@@ -1,48 +1,90 @@
-<!-- <script>
-	import Chart from "../components/Chart.svelte";
-</script>
-
-<Chart /> -->
-
 <script>
-	import { LayerCake, Svg } from 'layercake';
-	import { scaleLinear } from 'd3-scale';
+  import { LayerCake, Svg } from "layercake";
+  import { scaleLinear } from "d3-scale";
 
-	import Radar from '../components/Radar.svelte';
-	import AxisRadial from '../components/AxisRadial.svelte';
+  import Radar from "../components/Radar.svelte";
+  import AxisRadial from "../components/AxisRadial.svelte";
 
-	import data from '../components/radarScores.js';
+  import data from "../components/radarScores.js";
 
-	const seriesKey = 'name';
-	const xKey = ['fastball', 'change', 'slider', 'cutter', 'curve'];
+  const seriesKey = "name";
+  const xKey = ["fastball", "change", "slider", "cutter", "curve"];
 
-	const seriesNames = Object.keys(data[0]).filter(d => d !== seriesKey);
+  const seriesNames = Object.keys(data[0]).filter((d) => d !== seriesKey);
 
-	data.forEach(d => {
-		seriesNames.forEach(name => {
-			d[name] = +d[name];
-		});
-	});
+  data.forEach((d) => {
+    seriesNames.forEach((name) => {
+      d[name] = +d[name];
+    });
+  });
 </script>
 
-<style>
-	.chart-container {
-		width: 100%;
-		height: 600px;
-	}
-</style>
-
-<div class="chart-container">
-	<LayerCake
-		padding={{ top: 30, right: 30, bottom: 30, left: 30 }}
-		x={xKey}
-		xDomain={[0, 10]}
-		xRange={({ height }) => [0, height / 2]}
-		data={data}
-	>
-		<Svg>
-			<AxisRadial/>
-			<Radar/>
-		</Svg>
-	</LayerCake>
+<!--
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ]
+  }
+  ```
+-->
+<div class="container mx-auto mt-16">
+  <div class="md:grid md:grid-cols-3 md:gap-6">
+    <div class="md:col-span-1">
+      <div class="px-4 sm:px-0">
+        <h2 class="text-2xl font-bold leading-6 text-gray-900">Analysis</h2>
+        <p class="text-md text-gray-600 mt-5">
+          This is the type of poop you most enjoy based on your swipes.
+        </p>
+      </div>
+    </div>
+    <div class="mt-5 md:mt-0 md:col-span-2">
+      <form action="#" method="POST">
+        <!-- <div class="shadow sm:rounded-md sm:overflow-hidden"> -->
+        <!-- <div class="px-4 py-5 bg-white space-y-6 sm:p-6 "> -->
+        <div class="grid grid-cols-1 gap-6 ">
+          <div class="col-span-1 sm:col-span-1">
+            <!-- <div class="mt-1 flex rounded-md shadow-sm"> -->
+            <div class="chart-container">
+              <LayerCake
+                padding={{ top: 30, right: 30, bottom: 30, left: 30 }}
+                x={xKey}
+                xDomain={[0, 10]}
+                xRange={({ height }) => [0, height / 2]}
+                {data}
+              >
+                <Svg>
+                  <AxisRadial />
+                  <Radar />
+                </Svg>
+              </LayerCake>
+            </div>
+            <style>
+              .chart-container {
+                position: relative;
+                width: 100%;
+                height: 12rem;
+              }
+              @media (min-width: 48em) {
+                .chart-container {
+                  position: relative;
+                  width: 100%;
+                  height: 33vw;
+                }
+              }
+            </style><!-- </div> -->
+          </div>
+        </div>
+        <!-- </div> -->
+        <!-- </div> -->
+      </form>
+    </div>
+  </div>
 </div>
