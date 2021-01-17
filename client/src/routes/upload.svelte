a047c6ffbf2a20b1d7c4c5606d9048692e790807
Eric Y <eye@usc.edu>
2021-01-16 18:00:41 -0700

    Add upload modified swipe

---
+++ client/src/routes/upload.svelte
@@ -0,0 +1,26 @@
+<script>
+    function onSubmit(data){
+        validate_data(data)
+    }
+</script>
+
+<form>
+    <input type="image" />
+    <div class="flex flex-row">
+        <label for="yellow">Yellow</label>
+        <input type="radio" name="color" id= "yellow" />
+        <label for="green">Green</label>
+        <input type="radio" name="color" id= "green" />
+        <label for="brown">Brown</label>
+        <input type="radio" name="color" id= "brown" />
+        <label for="black">Black</label>
+        <input type="radio" name="color" id= "black" />
+    </div>
+    <div class = "flex flex-row">
+        <label for="smooth">Smooth</label>
+        <input type="radio" name="consistency" id="smooth" />
+        <label for="rough">Rough</label>
+        <input type="radio" name="consistency" id="rough" />
+    </div>
+    <input type="submit" value="Submit">
+</form>
